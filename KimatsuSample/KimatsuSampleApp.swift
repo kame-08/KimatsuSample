b57222d2ea8e970c6147cadb6320a00b1f9a8349
Ryo <21cm0108@jec.ac.jp>
2022-09-12 23:19:13 +0900

    Initial Commit

---
+++ KimatsuSample/KimatsuSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KimatsuSampleApp.swift
+//  KimatsuSample
+//
+//  Created by Ryo on 2022/09/12.
+//
+
+import SwiftUI
+
+@main
+struct KimatsuSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
